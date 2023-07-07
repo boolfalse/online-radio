7cfb7775edd287b57997e8c9707fe0dad25fe0a2
boolfalse <extravaganza1024@gmail.com>
2023-07-07 09:36:42 -0700

    Vite dotenv usage

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,23 @@
 
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import 'dotenv/config';
 
-export default ({ mode }) => {
-    const envVars = loadEnv(mode, process.cwd());
-
-    return defineConfig({
-        build: {
-            minify: 'esbuild',
-            cssMinify: true,
-        },
-        plugins: [react()],
-        server: {
-            port: parseInt(envVars.VITE_SERVER_PORT),
-            proxy: {
-                '/api': {
-                    target: `http://localhost:${envVars.VITE_BACKEND_PORT}`,
-                    changeOrigin: true,
-                    secure: false,
-                    // rewrite: (path) => path.replace(/^\/api/, '')
-                }
+export default defineConfig({
+    build: {
+        minify: process.env.APP_ENV === 'production' ? 'esbuild' : false,
+        cssMinify: process.env.APP_ENV === 'production',
+    },
+    plugins: [react()],
+    server: {
+        port: parseInt(process.env.VITE_SERVER_PORT),
+        proxy: {
+            '/api': {
+                target: `http://localhost:${process.env.VITE_BACKEND_PORT}`,
+                changeOrigin: true,
+                secure: process.env.APP_ENV === 'production',
+                // rewrite: (path) => path.replace(/^\/api/, '')
             }
-        },
-    });
-}
+        }
+    },
+});
